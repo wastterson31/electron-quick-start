8580c9145c06532552148df5a6db37d2b623e83f
waster ulloa <josewastterson@gmail.com>
2024-09-28 09:03:00 -0500

    Integrantes

--- renderer.js
+++ renderer.js
@@ -5,3 +5,23 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+console.log("hola");
+
+const button = document.querySelector("#btn-sm");
+const messageContainer = document.querySelector("#message-container");
+
+button.addEventListener("click", function() {
+    const teamMembers = ["LavaTuAuto", "CitaSalud", "Mecánico a Domicilio"];
+    const welcomeMessage = `Sistemas para realizar: ${teamMembers.join(', ')}`;
+    
+    const messageParagraph = document.createElement("p");
+    messageParagraph.textContent = welcomeMessage;
+    
+    messageContainer.innerHTML = "";
+    
+    messageContainer.appendChild(messageParagraph);
+    
+    messageParagraph.style.fontSize = "1.5em";
+    messageParagraph.style.color = "#333";
+    messageParagraph.style.marginTop = "20px";
+});
